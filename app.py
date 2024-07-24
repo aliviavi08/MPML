bf5232ffaf417ffb910775bab492d3922081b94a
aliviavi08 <22611016@students.uii.ac.id>
2024-07-25 00:09:27 +0700

    app.py

--- app.py
+++ app.py
@@ -16,16 +16,16 @@ def main():
     # Form for input
     with st.form(key='prediction_form'):
         gender = st.selectbox('Gender', ['Male', 'Female'])
-        marital_status = st.selectbox('Marital Status', ['Single', 'Married', 'Prefer Not to Say'])
+        marital_status = st.selectbox('Marital_Status', ['Single', 'Married', 'Prefer Not to Say'])
         occupation = st.selectbox('Occupation', ['Employee', 'Student', 'Self Employed', 'House Wife', 'Other'])
         monthly_income = st.selectbox('Monthly_Income', ['No Income', 'Below Rs.10000', '10001 to 25000', '25001 to 50000', 'More than 50000'])
-        educational_qualifications = st.selectbox('Educational Qualifications', ['Graduate', 'Post Graduate', 'Ph.D', 'School', 'Uneducated'])
+        educational_qualifications = st.selectbox('Educational_Qualifications', ['Graduate', 'Post Graduate', 'Ph.D', 'School', 'Uneducated'])
         feedback = st.selectbox('Feedback', ['Positive', 'Negative'])
         age = st.number_input('Age', min_value=0)
-        family_size = st.number_input('Family Size', min_value=1, max_value=10)
-        latitude = st.number_input('latitude')
-        longitude = st.number_input('longitude')
-        pin_code = st.number_input('Pin Code')
+        family_size = st.number_input('Family_Size', min_value=1, max_value=10)
+        latitude = st.number_input('Latitude')
+        longitude = st.number_input('Longitude')
+        pin_code = st.number_input('Pin_Code')
 
         submit_button = st.form_submit_button(label='Predict')
 
@@ -33,14 +33,14 @@ def main():
             # Convert inputs into a DataFrame with correct column names
             data = pd.DataFrame({
                 'Gender': [gender],
-                'Marital Status': [marital_status],
+                'Marital_Status': [marital_status],
                 'Occupation': [occupation],
-                'Monthly Income': [monthly_income],
-                'Educational Qualifications': [educational_qualifications],
-                'Family Size': [family_size],
-                'latitude': [latitude],
-                'longitude': [longitude],
-                'Pin Code': [pin_code],
+                'Monthly_Income': [monthly_income],
+                'Educational_Qualifications': [educational_qualifications],
+                'Family_Size': [family_size],
+                'Latitude': [latitude],
+                'Longitude': [longitude],
+                'Pin_Code': [pin_code],
                 'Feedback': [feedback]
             })
 
